b3b41b9ed4023fdf68b9b3d880b75c8087e99330
Bela <brstamenkovits@gmail.com>
2026-07-22 09:09:40 +0200

    database connection

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import streamlit as st
+from supabase import create_client, Client
 
 st.title("Hello World")
 st.write("This is a test")
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_ADMIN_KEY"]
+
+
+
+db = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
+
+
+def list_ingredients():
+    response = db.schema("app").table("ingredients").select("id, name").execute()
+    return response.data
+
+
+st.write(list_ingredients())
